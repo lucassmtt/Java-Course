746bb07d0e6605ae96128280b799ff05900b068d
Lucas Motta <110114441+lucassmtt@users.noreply.github.com>
2023-03-28 15:33:09 -0300

    .

--- untitled/src/ex04-Data_input/ex04/Main.java
+++ untitled/src/ex04-Data_input/ex04/Main.java
@@ -4,23 +4,32 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        String nome;
+        String name;
 
-        nome = sc.next();//vai scannear a string
-        System.out.println("Você digitou " + nome);
+        name = sc.next();//vai scannear a string
+        System.out.printf("Você digitou " + name + "%n");
 
         int num;
         num = sc.nextInt();//vai scannear um número inteiro
-        System.out.printf("Você digitou %d", num);
+        System.out.printf("Você digitou %d %n", num);
 
         double myNum;
         myNum = sc.nextDouble();//vai scannear seu número com ponto flutuante
-        System.out.printf("Você digitou o número %.2f", myNum);
+        System.out.printf("Você digitou o número %.2f %n", myNum);
 
         char myChar;
         myChar = sc.next().charAt(0);
-        System.out.printf("Você digitou o caracter %s", myChar);
+        System.out.printf("Você digitou o caracter %s %n", myChar);
+        System.out.printf("--------------------------------------------------------------- %n");
 
+        String other_Name, postname, address;
+
+        sc.nextLine();
+        other_Name = sc.nextLine();//vai processar todas na mesma linha
+        postname = sc.nextLine();
+        address = sc.nextLine();
+
+        System.out.printf("%s, %s, %s", other_Name, postname, address);
         sc.close();
     }
 
