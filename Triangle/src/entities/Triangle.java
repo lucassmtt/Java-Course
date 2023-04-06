3f755afbaf8e38a65458e349ee5207ab1d67f1d5
Lucas Motta <110114441+lucassmtt@users.noreply.github.com>
2023-04-05 21:01:27 -0300

    .

---
+++ Triangle/src/entities/Triangle.java
@@ -0,0 +1,12 @@
+package entities;
+
+public class Triangle{
+    
+    public double lado_a;
+    public double lado_b;
+    public double lado_c;
+
+    public static double main(lado_a,  lado_c, lado_b){
+
+    }
+}
