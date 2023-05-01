8e1c3a72c5354d4bc5231b1d6cdf42e9729f4bde
Lucas Motta <110114441+lucassmtt@users.noreply.github.com>
2023-05-01 15:58:30 -0300

    Exercises about matrices...

---
+++ WS-Java/Curso_programacao/Exercise_08_Matrices/src/Main.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Locale.setDefault(Locale.US);
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the number: ");
+        int n = input.nextInt();
+        int[][] mat = new int[n][n];
+
+        for (int row = 0; row < n; row++){
+            System.out.println("Please send the value for row " + row + ": ");
+            for (int collum = 0; collum < n; collum++){
+                mat[row][collum] = input.nextInt();
+            }
+        }
+
+//        for (int row = 0; row < n; row++) {
+//            for (int collum = 0; collum < n; collum++) {
+//                System.out.print(mat[row][collum] + " ");;
+//            }
+//            System.out.println();
+//        }
+
+        int negative_numbers = 0;
+        int[] main_diagonal = new int[n];
+
+        for (int row = 0; row < n; row++) {
+            for (int collum = 0; collum < n; collum++) {
+                if (row == collum){
+                    main_diagonal[row] = mat[row][collum];
+                }
+                int num = mat[row][collum];
+                if (num < 0) {
+                    negative_numbers += 1;
+                }
+            }
+        }
+        System.out.println("Main diagonal: ");
+        for (int i = 0; i < main_diagonal.length; i++){
+            System.out.print(main_diagonal[i] + " ");
+        }
+        System.out.println();
+        System.out.println("Negative numbers: ");
+        System.out.print(negative_numbers);
+        input.close();
+    }
+}
