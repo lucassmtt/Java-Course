5bd8aa310a74d15522049f70bffbad2f5203d38d
Lucas Motta <110114441+lucassmtt@users.noreply.github.com>
2023-05-02 17:26:53 -0300

    Update class_02.java

--- WS-Java/Curso_programacao/DatetimeInJava/src/class_02.java
+++ WS-Java/Curso_programacao/DatetimeInJava/src/class_02.java
@@ -1,6 +1,7 @@
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.Scanner;
@@ -9,6 +10,15 @@ public class class_02 {
     public static void main(String[] args){
         Locale.setDefault(Locale.US);
 
-        LocalDate d04 = LocalDate.parse("2002-04-22");
+        LocalDate formatDateTime = LocalDate.parse("2002-04-22");
+        DateTimeFormatter myformat = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        Instant d03 = Instant.parse("2002-04-22T01:20:24Z");
+
+        System.out.println("D01 = " + formatDateTime.format(myformat));
+        System.out.println("D02 = " + myformat.format(formatDateTime));
+
+        DateTimeFormatter myFormatInstant = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").withZone(ZoneId.systemDefault());
+
+        System.out.println("D03 = " + myFormatInstant.format(d03));
     }
 }
